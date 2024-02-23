dabd07cf656b4867f3a75a5e18d0b57a50cbf42a
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-02-23 18:46:31 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,97 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purple,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: const Text(''),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
